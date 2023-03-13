e7e96fbf323ceddadc23fb20657af161cbd018c6
takuto-honda <takuto.engineer@gmail.com>
2023-03-13 19:35:45 +0900

    ユーザーと記事テーブル作成

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name', 20);
+            $table->string('email', 255)->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password', 255);
+            $table->integer('age')->nullable();
+            $table->integer('gender')->nullable();
+            $table->string('picture', 255)->nullable();
+            $table->string('subject', 20)->nullable();
+            $table->string('club', 20)->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
