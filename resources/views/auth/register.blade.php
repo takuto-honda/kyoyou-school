10b79e49e3d7da998c5494ef7f623a291e5ff0eb
takuto-honda <takuto.engineer@gmail.com>
2023-03-15 10:39:53 +0900

    ユーザー登録画面作成

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,47 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
+@extends('app')
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+@section('title', 'ユーザー登録')
+
+@section('content')
+  <div class="container">
+    <div class="row">
+      <div class="mx-auto col col-12 col-sm-11 col-md-9 col-lg-7 col-xl-6">
+        <h1 class="text-center"><a class="text-dark" href="/">教YOU</a></h1>
+        <div class="card mt-3">
+          <div class="card-body text-center">
+            <h2 class="h3 card-title text-center mt-2">ユーザー登録</h2>
+
+            <div class="card-text">
+              <form method="POST" action="{{ route('register') }}">
+                @csrf
+                <div class="md-form">
+                  <label for="name">ユーザー名</label>
+                  <input class="form-control" type="text" id="name" name="name" required value="{{ old('name') }}">
+                  <small>英数字3〜16文字(登録後の変更はできません)</small>
+                </div>
+                <div class="md-form">
+                  <label for="email">メールアドレス</label>
+                  <input class="form-control" type="text" id="email" name="email" required value="{{ old('email') }}" >
+                </div>
+                <div class="md-form">
+                  <label for="password">パスワード</label>
+                  <input class="form-control" type="password" id="password" name="password" required>
+                </div>
+                <div class="md-form">
+                  <label for="password_confirmation">パスワード(確認)</label>
+                  <input class="form-control" type="password" id="password_confirmation" name="password_confirmation" required>
+                </div>
+                <button class="btn btn-block blue-gradient mt-2 mb-2" type="submit">ユーザー登録</button>
+              </form>
+
+              <div class="mt-0">
+                <a href="{{ route('login') }}" class="card-text">ログインはこちら</a>
+              </div>
+              
+            </div>
+          </div>
         </div>
-
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ml-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+      </div>
+    </div>
+  </div>
+@endsection
