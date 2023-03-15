3624261ba46a1e1535340e685e505cf1b4599874
takuto-honda <takuto.engineer@gmail.com>
2023-03-15 12:11:11 +0900

    記事ルーティングを追加

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ArticleController::class, 'index']);
 
+Route::resource('articles', ArticleController::class)
+->middleware(['auth'])->except(['index']);;
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
