5869b3ce24b84d65c79d59a12a32b7e5b5fd373b
takuto-honda <takuto.engineer@gmail.com>
2023-03-15 13:48:58 +0900

    投稿保存メソッド作成

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Article;
 use Illuminate\Http\Request;
+use App\Http\Requests\ArticleRequest;
 use App\Http\Controllers\ArticleController;
 
 class ArticleController extends Controller
@@ -17,5 +18,14 @@ class ArticleController extends Controller
     public function create()
     {
         return view('articles.create');
+    }
+
+    public function store(ArticleRequest $request, Article $article)
+    {
+        $article->title = $request->title;
+        $article->body = $request->body;
+        $article->user_id = $request->user()->id;
+        $article->save();
+        return redirect()->route('articles.index');
     }
 }
