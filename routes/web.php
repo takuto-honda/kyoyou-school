15b602dca70c4f56f0400cef8c8d8a54ae36568d
takuto-honda <takuto.engineer@gmail.com>
2023-03-16 17:36:30 +0900

    記事詳細ルーティング追加（authミドルウェアを外すため）

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ArticleController::class, 'index'])->name('articles.index');
 
-Route::resource('articles', ArticleController::class)
-->except(['index'])->middleware(['auth']);
+Route::resource('/articles', ArticleController::class)->except(['index', 'show'])->middleware(['auth']);
+Route::resource('/articles', ArticleController::class)->only(['show']);
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
