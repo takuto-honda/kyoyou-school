73abbdfa76233e3ee50ac82a4d738fc2d71e006a
takuto-honda <takuto.engineer@gmail.com>
2023-03-16 16:37:29 +0900

    記事テーブル一時修正

--- database/migrations/2023_03_13_190751_create_articles_table.php
+++ database/migrations/2023_03_13_190751_create_articles_table.php
@@ -16,12 +16,12 @@ return new class extends Migration
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
-            $table->string('subject', 15)->nullable();
-            $table->string('club', 15)->nullable();
-            $table->string('title', 255)->nullable(false)->change();
-            $table->text('body')->nullable(false)->change();
-            $table->string('image_url')->nullable(false)->change();
-            $table->tinyInteger('type')->unsigned();
+            // $table->string('subject', 15)->nullable();
+            // $table->string('club', 15)->nullable();
+            $table->string('title', 255)->nullable(false);
+            $table->text('body')->nullable(false);
+            // $table->string('image_url')->nullable(false);
+            // $table->tinyInteger('type')->unsigned();
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
