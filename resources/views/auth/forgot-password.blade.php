80ce7e41e25816c22f70865ad32306eec3948305
honda_takuto <takuto.engineer@gmail.com>
2023-03-17 16:50:59 +0900

    パスワード再設定画面作成

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,41 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+@extends('app')
+
+@section('title', 'パスワード再設定')
+
+@section('content')
+  <div class="container">
+    <div class="row">
+      <div class="mx-auto col col-12 col-sm-11 col-md-9 col-lg-7 col-xl-6">
+        <h1 class="text-center"><a class="text-dark" href="/">memo</a></h1>
+        <div class="card mt-3">
+          <div class="card-body text-center">
+            <h2 class="h3 card-title text-center mt-2">パスワード再設定</h2>
+
+            @include('error_card_list')
+
+            @if (session('status'))
+              <div class="card-text alert alert-success">
+                {{ session('status') }}
+              </div>
+            @endif
+
+            <div class="card-text">
+              <form method="POST" action="{{ route('password.email') }}">
+                @csrf
+
+                <div class="md-form">
+                  <label for="email">メールアドレス</label>
+                  <input class="form-control" type="text" id="email" name="email" required>
+                </div>
+
+                <button class="btn btn-block blue-gradient mt-2 mb-2" type="submit">メール送信</button>
+
+              </form>
+
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+  </div>
+@endsection
