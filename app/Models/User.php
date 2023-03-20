99b6d3bc980b19a4bc20e7de923227effe7ae051
honda_takuto <takuto.engineer@gmail.com>
2023-03-20 19:38:37 +0900

    ユーザーページに記事一覧を表示

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,12 @@ use App\Mail\BareMail;
 use App\Notifications\PasswordResetNotification;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Article;
 
 class User extends Authenticatable
 {
@@ -48,6 +50,11 @@ class User extends Authenticatable
     public function sendPasswordResetNotification($token)
     {
         $this->notify(new PasswordResetNotification($token, new BareMail()));
+    }
+
+    public function articles(): HasMany
+    {
+        return $this->hasMany(Article::class);
     }
 
     public function followers(): BelongsToMany
