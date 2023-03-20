d7555ac1116b38fdbf844ec7048399448f84246a
honda_takuto <takuto.engineer@gmail.com>
2023-03-20 20:10:34 +0900

    フォロー・フォロワーの一覧表示

--- resources/views/users/user.blade.php
+++ resources/views/users/user.blade.php
@@ -21,13 +21,13 @@
       </h2>
     </div>
     <div class="card-body">
-      <div class="card-text">
-        <a href="" class="text-muted">
-          {{ $user->count_followings }} フォロー
-        </a>
-        <a href="" class="text-muted">
-          {{ $user->count_followers }} フォロワー
-        </a>
-      </div>
+        <div class="card-text">
+          <a href="{{ route('users.followings', ['name' => $user->name]) }}" class="text-muted">
+            {{ $user->count_followings }} フォロー
+          </a>
+          <a href="{{ route('users.followers', ['name' => $user->name]) }}" class="text-muted">
+            {{ $user->count_followers }} フォロワー
+          </a>
+        </div>
     </div>
   </div>
