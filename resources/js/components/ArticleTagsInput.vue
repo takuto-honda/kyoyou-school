57a23f52f096e81d057e48ba4e9317ee36f21add
honda_takuto <takuto.engineer@gmail.com>
2023-03-21 21:09:34 +0900

    スペースキーでタグ確定

--- resources/js/components/ArticleTagsInput.vue
+++ resources/js/components/ArticleTagsInput.vue
@@ -10,6 +10,7 @@
         :tags="tags"
         placeholder="タグを5個まで入力できます"
         :autocomplete-items="filteredItems"
+        :add-on-key="[13, 32]"
         @tags-changed="newTags => tags = newTags"
       />
     </div>
@@ -65,5 +66,8 @@
       margin-right: 4px;
       border-radius: 0px;
       font-size: 13px;
+    }
+    .vue-tags-input .ti-tag::before {
+      content: "#";
     }
   </style>
